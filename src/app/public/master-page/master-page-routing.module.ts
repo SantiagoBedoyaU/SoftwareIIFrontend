d189df98e26b16fcce86756af19ad232cfc1f693
valeavila978 <116299483+valeavila978@users.noreply.github.com>
2024-11-16 14:23:23 -0500

    Updated code to make the design responsive, the responsive login and logout are missing

--- src/app/public/master-page/master-page-routing.module.ts
+++ src/app/public/master-page/master-page-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AfterViewInit } from '@angular/core';
 
+declare const M: any;
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MasterPageRoutingModule { }
+export class MasterPageRoutingModule { ngAfterViewInit() {
+  // Inicializar sidenav para dispositivos móviles
+  const elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems, { edge: 'right' }); // Alineado a la derecha
+}}
+
+
+
+
+
