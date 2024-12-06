e052edae4135785a078a7f797c7a2f287646048b
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2024-12-06 11:26:30 -0500

    second part of report generation (#30)

--- src/app/modelos/report.model.ts
+++ src/app/modelos/report.model.ts
@@ -11,3 +11,16 @@ export interface WaitingTimeReport {
     days_with_max_waiting_time: string;
     days_with_min_waiting_time: string;
 }
+
+export interface UsersDNIReport {
+    cc_users: number;
+    ti_users: number;
+    tp_users: number;
+    cc_percentage: number;
+    ti_percentage: number;
+    tp_percentage: number;
+}
+
+export interface ConsultedDoctorsReport {
+    doctors: Record<string, number>;
+}
